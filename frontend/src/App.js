0a3d351da26aa0c01473cb5006ef170f30d4d8bc
antonio.dellaflora <antonio.dellaflora@meta.com.br>
2020-04-11 18:40:49 -0300

    Modo noturno \o/

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { ApplyProvider } from './HigherOrder';
 import Routes from './routes';
 import { Header } from './Components';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 const App = () => {
+  const [theme, setTheme] = useState({
+    palette: {
+      type: 'light',
+    },
+  });
+
+  const toggleDarkTheme = () => {
+    let newPaletteType = theme.palette.type === 'light' ? 'dark' : 'light';
+
+    setTheme({
+      palette: {
+        type: newPaletteType,
+      },
+    });
+  };
+
+  const muiTheme = createMuiTheme(theme);
+
   return (
-    <>
-      <Header />
+    <MuiThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <Header
+        onToggleTheme={toggleDarkTheme}
+        isDarkTheme={theme.palette.type === 'dark'}
+      />
       <Routes />
-    </>
+    </MuiThemeProvider>
   );
 };
 
--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import useStyles from './styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Tooltip from '@material-ui/core/Tooltip';
+import LightIcon from '@material-ui/icons/Brightness4';
+import DarkIcon from '@material-ui/icons/Brightness7';
+import PropTypes from 'prop-types';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-export default function ButtonAppBar() {
+function ButtonAppBar({ onToggleTheme, isDarkTheme }) {
   const classes = useStyles();
+
+  function renderThemeToggler() {
+    return (
+      <Tooltip arrow title="Alternar tema claro/escuro">
+        <IconButton aria-label="toggle dark theme" onClick={onToggleTheme}>
+          {isDarkTheme ? <DarkIcon /> : <LightIcon className={classes.button} />}
+        </IconButton>
+      </Tooltip>
+    );
+  }
 
   return (
     <div className={classes.root}>
@@ -37,9 +39,21 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             News
           </Typography>
+          {renderThemeToggler()}
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
   );
 }
+
+ButtonAppBar.propTypes = {
+  onToggleTheme: PropTypes.func.isRequired,
+  isDarkTheme: PropTypes.bool,
+};
+
+ButtonAppBar.defaultProps = {
+  bool: false,
+};
+
+export default ButtonAppBar;
