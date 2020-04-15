6a283d121f155eaf761881ccc8d0beba9468b173
antonio.dellaflora <antonio.dellaflora@meta.com.br>
2020-04-15 14:00:03 -0300

    Erro no AutoComplete

--- frontend/src/Components/AutocompleteInput/AutocompleteInput.js
+++ frontend/src/Components/AutocompleteInput/AutocompleteInput.js
@@ -2,11 +2,20 @@ import React, { useEffect, useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
-const AutocompleteInput = ({ array, onChange, renderLabel, name, placeholder }) => {
+const AutocompleteInput = ({
+  array,
+  onChange,
+  renderLabel,
+  name,
+  placeholder,
+  error,
+}) => {
   const buildOnChange = (event, value) => {
     const { id } = value || {};
-
     const response = { target: { value: id, name } };
 
     onChange(response);
@@ -19,25 +28,31 @@ const AutocompleteInput = ({ array, onChange, renderLabel, name, placeholder }) 
   }, [array]);
 
   return (
-    <Autocomplete
-      key={key}
-      id={`Autocomplete-${name}`}
-      options={array}
-      // loading
-      loadingText="Carregando..."
-      onChange={buildOnChange}
-      getOptionLabel={renderLabel}
-      noOptionsText="Sem opções"
-      renderInput={params => (
-        <TextField
-          fullWidth
-          {...params}
-          name="ABC"
-          label={placeholder}
-          variant="outlined"
+    <FormControl error variant="outlined" fullWidth>
+      <Paper>
+        <Autocomplete
+          key={key}
+          id={`Autocomplete-${name}`}
+          options={array}
+          // loading
+          loadingText="Carregando..."
+          onChange={buildOnChange}
+          getOptionLabel={renderLabel}
+          noOptionsText="Sem opções"
+          renderInput={params => (
+            <TextField
+              fullWidth
+              error={!!error}
+              {...params}
+              name="ABC"
+              label={placeholder}
+              variant="outlined"
+            />
+          )}
         />
-      )}
-    />
+      </Paper>
+      <FormHelperText>{error}</FormHelperText>
+    </FormControl>
   );
 };
 
@@ -47,6 +62,7 @@ AutocompleteInput.propTypes = {
   renderLabel: PropTypes.func,
   placeholder: PropTypes.string,
   name: PropTypes.string,
+  error: PropTypes.string,
 };
 
 AutocompleteInput.defaultProps = {
@@ -54,6 +70,7 @@ AutocompleteInput.defaultProps = {
   renderLabel: () => 'Label',
   placeholder: '',
   name: '',
+  error: '',
 };
 
 export default AutocompleteInput;
