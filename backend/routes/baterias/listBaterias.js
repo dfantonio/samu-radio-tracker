e2ef79b3f91b5e68f5d38ad664c2e4796de028fd
antonio.dellaflora <antonio.dellaflora@meta.com.br>
2020-04-15 18:23:24 -0300

    Filtro por status nos rádios e baterias

--- backend/routes/baterias/listBaterias.js
+++ backend/routes/baterias/listBaterias.js
@@ -1,9 +1,16 @@
 const { Bem } = require('../../models');
 
 const listBaterias = async (req, res) => {
+  const { status } = req.query;
+  const where = {};
+
+  if (status) {
+    where.status_id = status;
+  }
   const response = await Bem.findAll({
     where: {
       tipo_id: 2,
+      ...where,
     },
   });
 
--- backend/routes/radios/listRadios.js
+++ backend/routes/radios/listRadios.js
@@ -1,9 +1,16 @@
 const { Bem } = require('../../models');
 
 const listRadios = async (req, res) => {
+  const { status } = req.query;
+  const where = {};
+
+  if (status) {
+    where.status_id = status;
+  }
   const response = await Bem.findAll({
     where: {
       tipo_id: 1,
+      ...where,
     },
   });
 
