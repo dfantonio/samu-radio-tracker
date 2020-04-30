daaacc8c3fa735fab3e15225ae96ff2cba4380b7
Antônio Della Flora <antoniodellaflora@gmail.com>
2020-04-30 13:56:42 -0300

    Updates

---
+++ precache-manifest.a90cb080f516343a183eeb7f4f21926d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03a2bdfa0d987a5e668c81b0bebad59",
+    "url": "/samu-radio-tracker/index.html"
+  },
+  {
+    "revision": "2aff4d36f68b4cfe3cb8",
+    "url": "/samu-radio-tracker/static/js/2.fc10413f.chunk.js"
+  },
+  {
+    "revision": "4d7ef388ded4c46d8f17e43a588896c3",
+    "url": "/samu-radio-tracker/static/js/2.fc10413f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "871d6e3276d295d8ab28",
+    "url": "/samu-radio-tracker/static/js/main.f1ce815e.chunk.js"
+  },
+  {
+    "revision": "9ef0d2d0f56fc238d058",
+    "url": "/samu-radio-tracker/static/js/runtime-main.1794f34b.js"
+  }
+]);
