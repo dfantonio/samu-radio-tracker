a574247960d90693fc7d19cc4376537316221db1
Antônio Della Flora <antoniodellaflora@gmail.com>
2020-05-02 23:29:17 -0300

    conexão com ssh

--- ssh/sample.sh
+++ ssh/sample.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 echo "entrei no script"
-echo "$SUPER_SECRET"
-echo $SUPER_SECRET
+echo "$SERVER_IP"
+echo $SERVER_IP
+
+# Cria o arquivo da chave SSH
+touch sshCredentials.pem
+echo -e "$SERVER_SSH" > sshCredentials.pem
+chmod 600 sshCredentials.pem
+
+sudo scp -i sshCredentials.pem -r teste $SERVER_USER@$SERVER_IP:/home/samu_gcloud/samu-radio-tracker/frontend/build/
+
 # gcloud secrets versions access
 # echo gcloud secrets versions access
