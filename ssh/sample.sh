835224b6f517a668f2ca6768d2949c54f4d14f30
Antônio Della Flora <antoniodellaflora@gmail.com>
2020-05-02 22:58:04 -0300

    mudança no teste do shell script

--- ssh/sample.sh
+++ ssh/sample.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
 echo "entrei no script"
-gcloud secrets versions access
-echo gcloud secrets versions access
+echo "${{ secrets.SERVER_IP }}"
+echo ${{ secrets.SERVER_IP }}
+# gcloud secrets versions access
+# echo gcloud secrets versions access
