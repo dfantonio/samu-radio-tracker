6477caafc4392759c0c4243b9aef7ca0453a88d0
Antônio Della Flora <antoniodellaflora@gmail.com>
2020-05-02 23:06:31 -0300

    teste com variáveis

--- ssh/sample.sh
+++ ssh/sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "entrei no script"
-echo "${{ secrets.SERVER_IP }}"
-echo ${{ secrets.SERVER_IP }}
+echo "$SUPER_SECRET"
+echo $SUPER_SECRET
 # gcloud secrets versions access
 # echo gcloud secrets versions access
